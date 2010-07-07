027b4f90ad9234df589ffac5c5701a8637eea822
Luke Kreczko <lkreczko@gmail.com>
2010-07-07 13:09:54 +0000

    initial import

--- exotictop/TLmacros/m3_toy/run.sh
+++
@@ -1,3 +0,0 @@
-cp ../../FullRun_btag_new_mc_mixture.root .
-root -q -b run_m3.C >& m3.txt
-root -q -b 'plot_nfit_pull.C'
--- exotictop/TLmacros/make_fits/run_fit_none.cc
+++
@@ -1,7 +0,0 @@
-{
-  gROOT->LoadMacro("fit_new.C");
-
-  // just plot
-  fit_none(false);
-  fit_none(true);
-}
